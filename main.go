47e0e492f0b32968c9b6bfa062ad3380a2fa7f2f
Sami Al Sabbir <samialzsabbir@gmail.com>
2023-01-18 01:14:47 +0600

    first go commit

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var conferenceName = "Go Conference"
+	var totalTicketCount uint = 50
+
+	fmt.Printf("Welcome to %v \n", conferenceName)
+	fmt.Printf("We have total %v tickets available \n", totalTicketCount)
+}
