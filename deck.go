8d4dde5ed69cf2e6e5fc2eee98f7c674b039491d
Sami Al Sabbir <samialzsabbir@gmail.com>
2023-01-18 19:52:49 +0600

    add integer print method

--- deck.go
+++ deck.go
@@ -7,9 +7,9 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamond", "Hearts", "Clubs"}
+	cardSuits := deck{"Spades", "Diamond", "Hearts", "Clubs"}
 
-	cardValues := []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
+	cardValues := deck{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
