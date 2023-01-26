c7ba716e4451bf50403be72fb5ebe88534e7db42
Sami Al Sabbir <samialzsabbir@gmail.com>
2023-01-27 03:56:57 +0600

    added os testing functions

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 
@@ -9,5 +12,22 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected 52 cards but got %v", len(d))
 	}
+
+}
+
+func TestSaveToLocalAndLoad(t *testing.T) {
+
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToLocal("_decktesting")
+
+	loadDeck := openCardsFromLocal("_decktesting")
+
+	if len(loadDeck) != 52 {
+		t.Errorf("Whoopsie!Expectd 52 got %v", len(loadDeck))
+	}
+
+	os.Remove("_decktesting")
 
 }
